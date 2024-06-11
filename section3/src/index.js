d35de636b94a16ddc44b49c723ab1c9d9caa361d
eungyeong0927 <bb060@naver.com>
2024-06-11 18:44:26 +0900

    node js 모듈 공부하기

--- section3/src/index.js
+++ section3/src/index.js
@@ -1,8 +1,15 @@
+// commonJS 방식
 // const moduleData = require("./math");
 // console.log(moduleData.add(1, 2));
 // console.log(moduleData.sub(1, 2));
 
 // const { add, sub } = require("./math");
+
+/////////////////////////////////////////////////////////
+
+// ESM 방식
+import mul from "./math.js";
 import { add, sub } from "./math.js";
 console.log(add(1, 2));
 console.log(sub(1, 2));
+console.log(mul(2, 3));
--- section3/src/math.js
+++ section3/src/math.js
@@ -1,12 +1,6 @@
 //math 모듈
 
-function add(a, b) {
-    return a + b;
-}
-function sub(a, b) {
-    return a - b;
-}
-
+// commonJS 방식
 /*
 module.exports = {
     // add: add,
@@ -16,5 +10,26 @@ module.exports = {
     sub,
 };
 */
+/////////////////////////////////////////////////////////////////////
 
+// ESM 방식
+export default function multiply(a, b) {
+    return a * b;
+}
+
+export function add(a, b) {
+    return a + b;
+}
+export function sub(a, b) {
+    return a - b;
+}
+
+/*
+function add(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
 export { add, sub };
+*/
